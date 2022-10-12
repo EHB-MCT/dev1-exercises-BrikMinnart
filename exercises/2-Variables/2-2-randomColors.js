ff153ebb1b11d5143048b720a8712f3023e8632d
Brik Minnart <brik.minnart@student.ehb.be>
2022-10-12 12:11:53 +0200

    2-2-randomColors

--- exercises/2-Variables/2-2-randomColors.js
+++ exercises/2-Variables/2-2-randomColors.js
@@ -8,17 +8,33 @@ let context = canvas.getContext("2d");
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-let size = 400;
-let randomX = Math.floor(Math.random()* (width - size));
-let randomY = Math.floor(Math.random()* (height - size));
-
-let r = Math.floor(Math.random()* 255);
-let g = Math.floor(Math.random()*255);
-let b = Math.floor(Math.random()*255);
+let size = 800;
+let sizeDif= 100;
+let pos = 50;
+let posDif = 50;
 
 
-console.log(r, g, b);
 
-let color = "rgb("+r+","+g+","+b+")";
-context.fillStyle = color
-context.fillRect(randomX,randomY,size,size);
+
+drawRectangles(pos,size);
+drawRectangles(pos+posDif,size-sizeDif);
+drawRectangles(pos+2*posDif,size-2*sizeDif);
+drawRectangles(pos+3*posDif,size-3*sizeDif);
+drawRectangles(pos+4*posDif,size-4*sizeDif);
+drawRectangles(pos+5*posDif,size-5*sizeDif);
+drawRectangles(pos+6*posDif,size-6*sizeDif);
+drawRectangles(pos+7*posDif,size-7*sizeDif);
+
+function drawRectangles(pos, size){
+    let r = Math.floor(Math.random()* 255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    
+    
+    console.log(r, g, b);
+    
+    let color = "rgb("+r+","+g+","+b+")";
+    context.fillStyle = color
+    
+context.fillRect(pos,pos,size,size);
+}
